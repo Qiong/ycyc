436437c90eef205d859368f7b0c2950de60cff8a
Qiong <qiongfei.seas@gmail.com>
2014-07-24 17:01:54 -0700

    equality

--- pchallengecom/ocr.py
+++ pchallengecom/ocr.py
@@ -1,6 +1,25 @@
 #http://www.pythonchallenge.com/pc/def/ocr.html
-#
+#http://www.pythonchallenge.com/pc/def/equality.html
+#http://www.pythonchallenge.com/pcc/def/equality.html 
 
 #
 #
-input = ""
+from operator import itemgetter 
+def main():
+	dit = {}
+	l = []
+	fo = open("ocrinput", 'r')
+	text = fo.read()
+	for char in text:
+		if char in dit.keys():
+			dit[char] = dit[char]+1
+		else:
+			dit[char] = 1
+			l.append(char)
+	#result = sorted(dit.items(), key = itemgetter(1))
+	#print result
+	print l
+
+
+if __name__ =='__main__':
+	main()
