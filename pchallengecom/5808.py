e05ec8824e16cd61e6afdae14040b4dac3643318
cyberyc <qiongfei.seas@gmail.com>
2014-07-29 21:27:44 -0700

    all

---
+++ pchallengecom/5808.py
@@ -0,0 +1,6 @@
+#
+#http://www.pythonchallenge.com/pc/return/5808.html
+#
+#
+#
+#
--- pchallengecom/bull.py
+++ pchallengecom/bull.py
@@ -1,6 +1,19 @@
 
 #
 #http://www.pythonchallenge.com/pc/return/bull.html
-#
+#http://www.pythonchallenge.com/pcc/return/5808.html
+
+
+#https://oeis.org/search?q=1%2c%2011%2c%2021%2c%201211%2c%20111221&sort=number
+#https://oeis.org/search?q=1%2C2%2C2%2C4%2C6%2C6%2C8%2C10&sort=&language=&go=Search
+
 from PIL import Image
 from PIL import ImageDraw
+
+# a = [1, 11, 21, 1211, 111221, 
+
+def main():
+	print [ x for x in addOne(10) ]
+
+if __name__=='__main__':
+	main()
