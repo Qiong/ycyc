af94593e1986a2a4f62d1219e383204f1020a5e4
Gary <ynchen@685b35bfa4ce.ant.amazon.com>
2014-08-01 08:35:58 -0700

    pw

---
+++ pwd.py
@@ -0,0 +1,23 @@
+# PWD Generator
+
+import random
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num = "1234567890"
+
+pw_length = 6
+
+
+
+def main():
+	mypw = ""
+	for i in range(pw_length):
+		next_index = random.randrange(len(alphabet))
+		mypw = mypw + alphabet[next_index]
+	print mypw
+	u = random.randrange(upper)
+	n = random.randrange(num)
+
+if __name__ == '__main__':
+	main()
